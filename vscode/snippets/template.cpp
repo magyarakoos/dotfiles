5950ac568490d296ba89ba7937bf7a65d62fd8a6
LordOfPumpkins <ninjaoffical08@gmail.com>
2024-02-17 14:23:41 +0100

    arch descktop sync: 2024-02-17 14:23:41

---
+++ vscode/snippets/template.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define size(v) (int)v.size()
+
+using namespace std;
+using ll = long long;
+
+int main() {
+    cin.tie(0), ios::sync_with_stdio(0);
+
+}
