a19fa1ed8b61a4655c214087c254740c4e211ab0
magyarakoos <ninjaoffical08@gmail.com>
2024-05-01 16:11:13 +0200

    arch descktop sync: 2024-05-01 16:11:13

--- shell/cppenv.sh
+++ shell/cppenv.sh
@@ -4,13 +4,13 @@ clear
 g++ -o main main.cpp -std=c++20 -fsanitize=undefined -fsanitize=address -Wall -Wshadow 
 
 # Run the program and capture both output and time
-{ time ./main < ../input; } 2>&1 | tee output.txt
+{ /usr/bin/time -f "%e" ./main < ../input; } 2> output.txt | tee -a output.txt
 
 # Extract real time from output
-real_time=$(grep real output.txt | awk '{print $2}')
+real_time=$(tail -n 1 output.txt)
 
 # Convert real time to milliseconds
-real_time_ms=$(echo "$real_time" | sed 's/m//;s/s//' | awk -F 'm|s' '{printf "%.0f\n", ($1 * 60 + $2) * 1000}')
+real_time_ms=$(awk -v rtime="$real_time" 'BEGIN{ printf "%.0f\n", rtime * 1000 }')
 
 # Display time taken
 echo "Time taken: ${real_time_ms}ms"
