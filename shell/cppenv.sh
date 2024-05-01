26d7ba13d4f4544eff15e538110374f532f620d7
magyarakoos <ninjaoffical08@gmail.com>
2024-05-01 15:59:33 +0200

    arch descktop sync: 2024-05-01 15:59:33

--- shell/cppenv.sh
+++ shell/cppenv.sh
@@ -1,1 +1,15 @@
-cppenv.sh
+#!/bin/bash
+
+clear
+g++ -o main main.cpp -std=c++20 -fsanitize=undefined -fsanitize=address -Wall -Wshadow 
+
+time ./main < ../input && rm ./main
+
+output=$(time ./main < ../input)
+
+real_time=$(echo "$output" | grep real | awk '{print $2}')
+
+real_time_ms=$(echo "$real_time" | sed 's/m//;s/s//' | awk -F 'm|s' '{printf "%.0f\n", ($1 * 60 + $2) * 1000}')
+
+echo "Time taken: ${real_time_ms}ms
+
