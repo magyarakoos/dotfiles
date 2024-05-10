94eb014b16df18508a6306eccbe35dc4a4827462
magyarakoos <ninjaoffical08@gmail.com>
2024-05-10 15:58:59 +0200

    arch descktop sync: 2024-05-10 15:58:59

--- vscode/snippets/debugger.cpp
+++ vscode/snippets/debugger.cpp
@@ -3,5 +3,5 @@ using namespace std;
 
 template<class T, class... Args>
 void debug(T arg, Args... args) {
-    decltype(std::begin(std::declval<T>()), std::end(std::declval<T>()), std::true_type{});
+    decltype(begin(declval<T>()), end(declval<T>()), true_type{});
 }
