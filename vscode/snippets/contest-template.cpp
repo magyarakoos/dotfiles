f0b152daf9bad5862ecc7dcdb06bc58f545d1058
magyarakoos <ninjaoffical08@gmail.com>
2024-05-10 15:14:52 +0200

    arch descktop sync: 2024-05-10 15:14:52

---
+++ vscode/snippets/contest-template.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(v) (int)v.size()
+#define smin(x, y) x = min(x, y)
+#define smax(x, y) x = max(x, y)
+
+const ll INF = 2e9, MAXN = 2e5, MOD = 1e9 + 7;
+
+#define cases 0
+void solve() {
+    
+}
+
+int main() {
+    cin.tie(0), ios::sync_with_stdio(0);
+    int T = 1;
+    if (cases) cin >> T;
+    while (T--) solve();
+}
