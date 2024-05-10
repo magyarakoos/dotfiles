47a84f185851072bf1edf8b0f11b09fa0d03eaa2
magyarakoos <ninjaoffical08@gmail.com>
2024-05-10 16:02:25 +0200

    arch descktop sync: 2024-05-10 16:02:25

--- vscode/snippets/debugger.cpp
+++ vscode/snippets/debugger.cpp
@@ -1,7 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define VARNAME(x) #x
 template<class T, class... Args>
 void debug(T arg, Args... args) {
     bool can_iter = decltype(begin(declval<T>()), end(declval<T>()), true);
+    if (can_iter) {
+    } else {
+
+    }
+
+    debug(args);
 }
