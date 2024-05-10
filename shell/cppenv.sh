06df27d9d125c863b339531b894b11e5e95c8600
magyarakoos <ninjaoffical08@gmail.com>
2024-05-10 15:52:06 +0200

    arch descktop sync: 2024-05-10 15:52:06

--- shell/cppenv.sh
+++ shell/cppenv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 clear
-g++ -o main *main.cpp -std=c++20 -fsanitize=undefined,address -Wall -Wshadow 
+g++ -o main *main.cpp -std=c++20 -fsanitize=undefined,address -Wall -Wshadow -DLOCAL
 
 ./main < ../input
 
