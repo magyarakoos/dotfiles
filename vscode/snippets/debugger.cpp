85e9a45c02db532975f031f1e8dda6201137a2ec
magyarakoos <ninjaoffical08@gmail.com>
2024-05-10 16:06:51 +0200

    arch descktop sync: 2024-05-10 16:06:51

--- vscode/snippets/debugger.cpp
+++ vscode/snippets/debugger.cpp
@@ -1,14 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define VARNAME(x) #x
 template<class T, class... Args>
-void debug(T arg, Args... args) {
+void debug(T arg, string arg_name, Args... args) {
+    #ifndef LOCAL
+        return;
+    #endif
+
+    cout << "[ " << arg_name << " : ";
+
     bool can_iter = decltype(begin(declval<T>()), end(declval<T>()), true);
     if (can_iter) {
+        for (const auto& item : arg) {
+
+        }
     } else {
 
     }
 
+    cout << " ]\n";
     debug(args);
 }
