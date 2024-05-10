f87dae391b6185d19433573174d0649642123938
magyarakoos <ninjaoffical08@gmail.com>
2024-05-10 16:28:00 +0200

    arch descktop sync: 2024-05-10 16:28:00

--- vscode/snippets/debugger.cpp
+++ vscode/snippets/debugger.cpp
@@ -2,33 +2,41 @@
 using namespace std;
 
 template<class T>
-void debug_print(T arg, string arg_name) {
+void debug(T arg, string arg_name) {
     cerr << "[ " << arg_name << " : ";
     if (decltype(begin(declval<T>()), end(declval<T>()), true)) {
         for (const auto& item : arg) {
             cerr << "   [ " << item << " ]\n";
         }
     } else {
-        cerr << item;
+        cerr << arg;
     }
     cerr << " ]" << endl;
 }
 
-void debug() {
-    #ifndef LOCAL 
+template<class T>
+void debug(T arg, string labels[]) {
+    #ifndef LOCAL
         return;
     #endif
-    
+
+    cerr << "[ ";
+    for (int i = 0; i < sizeof(labels); ++i) {
+        debug(arg, labels[i]);
+    }
+    cerr << " ]" << endl;
 }
+
 #define DEBUG(...) \
-    #ifdef LOCAL \
-     do { \
-    string labels[] = {__VA_ARGS__}; \
-    debug(__VA_ARGS__, labels); \
+do { \
+    string labels[] = {#__VA_ARGS__}; \
+    debug(labels, __VA_ARGS__); \
 } while(0)
+
 
 int main() {
     float PI = acos(-1.0);
     int x = 135;
     vector<int> list = {1, 5, 82, 5, 7};
+    DEBUG(PI, x, list);
 }
