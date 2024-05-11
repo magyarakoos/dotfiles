746cc12d67301b2c949e03f348700e163a8fde94
magyarakoos <ninjaoffical08@gmail.com>
2024-05-11 08:46:46 +0200

    arch descktop sync: 2024-05-11 08:46:46

--- vscode/snippets/debugger.cpp
+++ vscode/snippets/debugger.cpp
@@ -2,22 +2,13 @@
 using namespace std;
 
 template<class T>
-struct has_begin_end {
-    template<class U>
-    static auto test(int) -> decltype(std::begin(std::declval<U>()) == std::end(std::declval<U>()), std::true_type());
-    template<class U>
-    static std::false_type test(...);
-    static constexpr bool value = std::is_same<decltype(test<T>(0)), std::true_type>::value;
-};
-
-template<class T>
 void debug(T arg, string arg_name) {
     cerr << "[ " << arg_name << " : ";
-    if (has_begin_end<T>::value) {
+    try {
         for (auto item : arg) {
             cerr << "   [ " << item << " ]\n";
         }
-    } else {
+    } catch {
         cerr << arg;
     }
     cerr << " ]" << endl;
