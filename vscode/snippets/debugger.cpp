d98625693e7f92d361797d76d834012b56f3a207
magyarakoos <ninjaoffical08@gmail.com>
2024-05-11 08:49:12 +0200

    arch descktop sync: 2024-05-11 08:49:12

--- vscode/snippets/debugger.cpp
+++ vscode/snippets/debugger.cpp
@@ -4,17 +4,19 @@ using namespace std;
 template<class T>
 void debug(T arg, string arg_name) {
     cerr << "[ " << arg_name << " : ";
-    try {
-        for (auto item : arg) {
-            cerr << "   [ " << item << " ]\n";
-        }
-    } catch {
-        try {
-            cerr << arg;
-        } catch {
-            cerr << "Could not print out value `" << arg_name << "`.\n";
-        }
+    
+    // if it supports for, run this
+    for (auto item : arg) {
+        cerr << "   [ " << item << " ]\n";
     }
+
+    // if not, run this
+    cerr << arg;
+    
+    // if it can't be printed, run this
+    cerr << "Could not print out value `" << arg_name << "`.\n";
+
+
     cerr << " ]" << endl;
 }
 #define DEBUG(...) debug(__VA_ARGS__, #__VA_ARGS__)
