9d5c7abab0bfaf382f2f63a77023937b1f595d45
magyarakoos <ninjaoffical08@gmail.com>
2024-05-11 09:20:39 +0200

    arch descktop sync: 2024-05-11 09:20:39

--- vscode/snippets/debugger.cpp
+++ vscode/snippets/debugger.cpp
@@ -26,17 +26,17 @@ typename enable_if<is_iterable<T>::value>::type debug(T arg, string arg_name) {
 }
 
 #define DEBUG(x) debug(x, #x)
-
 #define DB1(x) DEBUG(x)
 #define DB2(x, ...) DEBUG(x), DB1(__VA_ARGS__)
 #define DB3(x, ...) DEBUG(x), DB2(__VA_ARGS__)
-
-#define GET_MACRO(_1, _2, _3, NAME, ...) NAME
-#define DB(...) GET_MACRO(__VA_ARGS__, DB3, DB2, DB1)(__VA_ARGS__)
+#define DB4(x, ...) DEBUG(x), DB3(__VA_ARGS__)
+#define DB5(x, ...) DEBUG(x), DB4(__VA_ARGS__)
+#define GET_MACRO(_1, _2, _3, _4, _5, NAME, ...) NAME
+#define DB(...) GET_MACRO(__VA_ARGS__, DB5, DB4, DB3, DB2, DB1)(__VA_ARGS__)
 
 int main() {
     float PI = acos(-1.0);
     int x = 135;
     vector<int> list = {1, 5, 82, 5, 7};
-    DB(list);
+    DB(list, x, PI);
 }
