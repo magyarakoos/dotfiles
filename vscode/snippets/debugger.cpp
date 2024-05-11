3633a8315926c6a81082c6b7f5bb52783da90e7f
magyarakoos <ninjaoffical08@gmail.com>
2024-05-11 09:24:55 +0200

    arch descktop sync: 2024-05-11 09:24:55

--- vscode/snippets/debugger.cpp
+++ vscode/snippets/debugger.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#pragma region DEBUG
 template<typename T>
 class is_iterable {
     template<typename U> static auto test(U* p) -> decltype(begin(*p), end(*p), true_type{});
@@ -8,14 +9,12 @@ class is_iterable {
 public:
     static constexpr bool value = decltype(test<T>(nullptr))::value;
 };
-
 template<typename T>
 typename enable_if<!is_iterable<T>::value>::type debug(T arg, string arg_name) {
     cerr << "[ " << arg_name << " : ";
     cerr << fixed << setprecision(15) << arg;
     cerr << " ]" << endl;
 }
-
 template<typename T>
 typename enable_if<is_iterable<T>::value>::type debug(T arg, string arg_name) {
     cerr << "[ " << arg_name << " : ";
@@ -24,7 +23,6 @@ typename enable_if<is_iterable<T>::value>::type debug(T arg, string arg_name) {
     }
     cerr << "]" << endl;
 }
-
 #define DEBUG(x) debug(x, #x)
 #define DB1(x) DEBUG(x)
 #define DB2(x, ...) DEBUG(x), DB1(__VA_ARGS__)
@@ -33,6 +31,8 @@ typename enable_if<is_iterable<T>::value>::type debug(T arg, string arg_name) {
 #define DB5(x, ...) DEBUG(x), DB4(__VA_ARGS__)
 #define GET_MACRO(_1, _2, _3, _4, _5, NAME, ...) NAME
 #define DB(...) GET_MACRO(__VA_ARGS__, DB5, DB4, DB3, DB2, DB1)(__VA_ARGS__)
+#pragma endregion
+
 int main() {
     float PI = acos(-1.0);
     int x = 135;
