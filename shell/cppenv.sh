c4a86e9920d5caafe84627f1c747255902b69f5f
magyarakoos <ninjaoffical08@gmail.com>
2024-05-28 17:43:37 +0200

    arch desktop sync: 2024-05-28 17:43:37

--- shell/cppenv.sh
+++ shell/cppenv.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+current_dir=$(pwd)
+required_prefix=~/repos/contest-prog/
+if [[ "$current_dir" != $required_prefix* ]]; then
+    exit 0
+fi
+
 clear
 g++ -o main *main.cpp -std=c++20 -fsanitize=undefined,address -Wall -Wshadow -DLOCAL -I/home/akos/cpp-include/
 
