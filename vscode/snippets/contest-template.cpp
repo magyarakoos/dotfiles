3445c117e71ef17adbb9ccd3282a932d0d2ad11f
magyarakoos <magyar.kende.22f@gmail.com>
2024-07-30 16:26:13 +0200

    sync: 2024-07-30 16:26:13

--- vscode/snippets/contest-template.cpp
+++ vscode/snippets/contest-template.cpp
@@ -4,14 +4,13 @@
 #else
 #define DB(...)
 #endif
+
 using namespace std;
 using ll = long long;
 
 #define all(v) v.begin(), v.end()
 #define rall(v) v.rbegin(), v.rend()
 #define sz(v) (int)v.size()
-#define smin(x, y) x = min(x, y)
-#define smax(x, y) x = max(x, y)
 
 const ll INF = 2e9, MAXN = 2e5, MOD = 1e9 + 7;
 
