7ff8c34579ff6d15ff79d8f03e1761669bd09c52
magyarakoos <magyar.kende.22f@gmail.com>
2024-08-13 12:56:35 +0200

    sync: 2024-08-13 12:56:35

--- cpp-utility/debug.h
+++ cpp-utility/debug.h
@@ -12,24 +12,42 @@ public:
 };
 
 template<typename T>
+typename enable_if<!is_iterable<T>::value>::type debug(T arg) {
+    cerr << fixed << setprecision(15) << arg << " ";
+}
+
+template<typename T>
 typename enable_if<!is_iterable<T>::value>::type debug(T arg, string arg_name) {
-    cerr << "[ " << arg_name << " : " << fixed << setprecision(15) << arg << " ]" << endl;
+    cerr << "  [ " << arg_name << " : " << fixed << setprecision(15) << arg << " ]" << endl;
+}
+
+template<typename T>
+typename enable_if<is_iterable<T>::value>::type debug(T arg) {
+    cerr << "[ ";
+    for (const auto& item : arg) {
+        debug(item);
+    }
+    cerr << "]";
 }
 
 template<typename T>
 typename enable_if<is_iterable<T>::value>::type debug(T arg, string arg_name) {
-    cerr << "[ " << arg_name << " : ";
+    cerr << "  [ " << arg_name << " : ";
     for (const auto& item : arg) {
-        cerr << fixed << setprecision(15) << item << " ";
+        debug(item);
     }
     cerr << "]" << endl;
 }
 
 #define DEBUG(x) debug(x, #x)
 #define DB1(x) DEBUG(x)
-#define DB2(x, ...) DEBUG(x), DB1(__VA_ARGS__)
-#define DB3(x, ...) DEBUG(x), DB2(__VA_ARGS__)
-#define DB4(x, ...) DEBUG(x), DB3(__VA_ARGS__)
-#define DB5(x, ...) DEBUG(x), DB4(__VA_ARGS__)
+#define DB2(x, ...) DEBUG(x); DB1(__VA_ARGS__)
+#define DB3(x, ...) DEBUG(x); DB2(__VA_ARGS__)
+#define DB4(x, ...) DEBUG(x); DB3(__VA_ARGS__)
+#define DB5(x, ...) DEBUG(x); DB4(__VA_ARGS__)
 #define GET_MACRO(_1, _2, _3, _4, _5, NAME, ...) NAME
-#define DB(...) GET_MACRO(__VA_ARGS__, DB5, DB4, DB3, DB2, DB1)(__VA_ARGS__)
+#define DB(...) do { \
+    cerr << "[\n"; \
+    GET_MACRO(__VA_ARGS__, DB5, DB4, DB3, DB2, DB1)(__VA_ARGS__); \
+    cerr << "]\n"; \
+} while (0);
