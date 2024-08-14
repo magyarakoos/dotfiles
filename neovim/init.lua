2ef906788f7468c055b847a168c239b27f325134
magyarakoos <magyar.kende.22f@gmail.com>
2024-08-14 16:06:01 +0200

    sync: 2024-08-14 16:06:01

--- neovim/init.lua
+++ neovim/init.lua
@@ -868,8 +868,8 @@ require('lazy').setup({
 })
 
 -- Resize windows using Ctrl + Arrow keys
-vim.api.nvim_set_keymap('n', '<C-Up>', ':resize +2<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-Down>', ':resize -2<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-Up>', ':resize -2<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-Down>', ':resize +2<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-Left>', ':vertical resize -2<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-Right>', ':vertical resize +2<CR>', { noremap = true, silent = true })
 
