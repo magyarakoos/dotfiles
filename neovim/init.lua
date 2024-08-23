33ed9cb2469164ae3222f6d2a82427a408457da3
magyarakoos <magyar.kende.22f@gmail.com>
2024-08-23 13:21:01 +0200

    sync: 2024-08-23 13:21:01

--- neovim/init.lua
+++ neovim/init.lua
@@ -483,6 +483,7 @@ require('lazy').setup({
                 texlab = {},
                 pyright = {},
                 julials = {},
+                typescript = {},
                 -- rust_analyzer = {},
                 -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
                 --
