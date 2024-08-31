cb5594fc9d1d95f2d242db6b3aec22eca22192b5
magyarakoos <ninjaoffical08@gmail.com>
2024-08-31 12:59:01 +0200

    sync: 2024-08-31 12:59:01

---
+++ cpp-utility/contest-template.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#ifdef LOCAL
+#include <debug.h>
+#else
+#define DB(...)
+#endif
+
+using namespace std;
+using ll = long long;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(v) (int)v.size()
+
+const ll INF = 2e9, MAXN = 2e5, MOD = 1e9 + 7;
+
+#define cases 0
+void solve() {}
+
+int main() {
+    cin.tie(0), ios::sync_with_stdio(0);
+    int T = 1;
+    if (cases) cin >> T;
+    while (T--) solve();
+}
+
