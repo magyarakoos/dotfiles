c871ad7e46e5b356bfae61b22ce4551b4a1e03e7
magyarakoos <ninjaoffical08@gmail.com>
2024-10-29 15:08:00 +0100

    sync: 2024-10-29 15:08:00

--- cpp-utility/contest-template.cpp
+++ cpp-utility/contest-template.cpp
@@ -1,18 +1,8 @@
 #include <bits/stdc++.h>
-#ifdef LOCAL
-#include <debug.h>
-#else
-#define DB(...)
-#endif
 
 using namespace std;
-using ll = long long;
-
-#define all(v) v.begin(), v.end()
-#define rall(v) v.rbegin(), v.rend()
-#define sz(v) (int)v.size()
-
-const ll INF = 2e9, MAXN = 2e5, MOD = 1e9 + 7;
+using i32 = int;
+using i64 = long long;
 
 #define cases 0
 void solve() {}
@@ -23,4 +13,3 @@ int main() {
     if (cases) cin >> T;
     while (T--) solve();
 }
-
