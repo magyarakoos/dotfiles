9f60df45001d1bc6db18aa766a2efbc8612eba31
magyarakoos <magyar.kende.22f@gmail.com>
2024-11-28 08:57:01 +0100

    sync: 2024-11-28 08:57:01

--- neovim/init.lua
+++ neovim/init.lua
@@ -497,7 +497,6 @@ require('lazy').setup({
             --  - settings (table): Override the default settings passed when initializing the server.
             --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
             local servers = {
-                uiua = {},
                 clangd = {},
                 -- gopls = {},
                 svelte = {},
