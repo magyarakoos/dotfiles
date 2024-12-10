1fba130f149ad1e7c36b5490b8c9d986c7d46c1f
magyarakoos <magyar.kende.22f@gmail.com>
2024-12-10 14:03:01 +0100

    sync: 2024-12-10 14:03:01

--- neovim/nvim/lua/custom-commands.lua
+++ neovim/nvim/lua/custom-commands.lua
@@ -1,14 +1,5 @@
--- Check if the current file is a .uiua file
-function Fmt_command()
-    -- Save the current file
-    vim.cmd("write")
-
-    if vim.bo.filetype == "uiua" then
-        vim.cmd("!uiua fmt")
-    else
-        vim.cmd("normal! <Leader>fm")
-    end
-end
+-- Automagically format on save
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
 
 -- Map the custom command to <Leader>fm
 vim.api.nvim_set_keymap("n", "<Leader>fm", ":lua Fmt_command()<CR>", { noremap = true, silent = true })
