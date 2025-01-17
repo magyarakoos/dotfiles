82734bc62f4a05390f015790a606e225ae7b0a98
khun <khunor22@gmail.com>
2025-01-17 13:30:02 +0100

    i had to google how to make a pr and i also accidentally nuked my own config for this (dw it was on git so it wasnt an issue)

--- neovim/nvim/lua/options.lua
+++ neovim/nvim/lua/options.lua
@@ -1,36 +1,40 @@
 vim.api.nvim_create_autocmd("VimEnter", {
-    callback = function()
-        if vim.fn.argc() == 0 then
-            vim.cmd("Telescope find_files")
-        end
-    end,
+	callback = function()
+		if vim.fn.argc() == 0 then
+			vim.cmd("Telescope find_files")
+		end
+	end,
 })
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-    callback = function()
-        vim.highlight.on_yank()
-    end,
-    group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
-    pattern = "*",
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
+	pattern = "*",
 })
 
 vim.filetype.add({ extension = { ua = "uiua", lil = "lil" } })
 
 local ft_opts = {
-    uiua = function()
-        vim.o.spell = false
-    end,
+	uiua = function()
+		vim.o.spell = false
+	end,
 }
 
 local ft_opts_group = vim.api.nvim_create_augroup("FileOpts", { clear = true })
 
 for ft, opts in pairs(ft_opts) do
-    vim.api.nvim_create_autocmd("FileType", {
-        pattern = ft,
-        callback = opts,
-        group = ft_opts_group,
-    })
+	vim.api.nvim_create_autocmd("FileType", {
+		pattern = ft,
+		callback = opts,
+		group = ft_opts_group,
+	})
 end
+
+-- set commentsring to // for c and cpp files
+vim.cmd("autocmd FileType c setlocal commentstring=//\\ %s")
+vim.cmd("autocmd FileType cpp setlocal commentstring=//\\ %s")
 
 vim.o.ignorecase = true
 vim.o.smartcase = true
