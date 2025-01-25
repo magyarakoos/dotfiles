3787b45c1e405b1b3563ae3e0fc1554046827aac
magyarakoos <magyar.kende.22f@gmail.com>
2025-01-25 18:40:01 +0100

    sync: 2025-01-25 18:40:01

--- neovim/nvim/templates/contest-template.cpp
+++ neovim/nvim/templates/contest-template.cpp
@@ -1,23 +1,13 @@
 #include <bits/stdc++.h>
-#ifdef LOCAL
-#include <debug.h>
-#else
-#define DB(...)
-#endif
-
 using namespace std;
-using ll = long long;
+#define int int64_t
 
-#define all(v) v.begin(), v.end()
-#define rall(v) v.rbegin(), v.rend()
-#define sz(v) (int)v.size()
-
-const ll INF = 2e9, MAXN = 2e5, MOD = 1e9 + 7;
+const int INF = 2e9, MAXN = 2e5, MOD = 1e9 + 7;
 
 #define cases 0
 void solve() {}
 
-int main() {
+int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
     int T = 1;
     if (cases) cin >> T;
