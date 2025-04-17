e2388d0797b445c0ff10b69dc40423c28f8686e1
magyarakoos <ninjaoffical08@gmail.com>
2025-04-17 15:32:00 +0200

    sync: 2025-04-17 15:32:00

--- neovim/nvim/lua/treesitter-setup.lua
+++ neovim/nvim/lua/treesitter-setup.lua
@@ -38,6 +38,7 @@ vim.defer_fn(function()
             'markdown',
             'markdown_inline',
             'toml',
+            'jinja2'
         },
 
         -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
