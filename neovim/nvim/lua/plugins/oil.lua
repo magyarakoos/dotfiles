14e79f96e3e62b38d7432dc5a4d3afe16d2fdac3
magyarakoos <ninjaoffical08@gmail.com>
2025-06-01 19:25:00 +0200

    sync: 2025-06-01 19:25:00

--- neovim/nvim/lua/plugins/oil.lua
+++ neovim/nvim/lua/plugins/oil.lua
@@ -6,6 +6,7 @@ return {
         keymaps = { ['<leader>q'] = 'actions.close' },
         float = { max_width = 50, max_height = 20 },
         delete_to_trash = true,
+        show_hidden = true,
     },
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     keys = {
