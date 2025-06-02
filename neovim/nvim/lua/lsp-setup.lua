8c11ea362b8f29fab570d2ce5b657568537d06a5
magyarakoos <magyar.kende.22f@gmail.com>
2025-06-02 11:44:00 +0200

    sync: 2025-06-02 11:44:00

--- neovim/nvim/lua/lsp-setup.lua
+++ neovim/nvim/lua/lsp-setup.lua
@@ -77,7 +77,7 @@ local servers = {
     rust_analyzer = {},
     tailwindcss = {
         filetypes = { 'html', 'htmldjango' },
-        root_dir = util.root_pattern('tailwind.config.js'),
+        root_dir = util.root_pattern('tailwind.css'),
     },
 
     clangd = { filetypes = { 'c', 'cpp' } },
