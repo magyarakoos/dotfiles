9efedc204a804209cf2dfeb641a885e80f0f4ae5
magyarakoos <magyar.kende.22f@gmail.com>
2025-06-12 09:09:01 +0200

    sync: 2025-06-12 09:09:01

--- neovim/nvim/lua/lsp-setup.lua
+++ neovim/nvim/lua/lsp-setup.lua
@@ -87,6 +87,23 @@ local servers = {
 
     clangd = { filetypes = { 'c', 'cpp' } },
 
+    lua_ls = {
+        settings = {
+            Lua = {
+                runtime = { version = 'LuaJIT' },
+                diagnostics = {
+                    globals = { 'vim' },
+                },
+                workspace = {
+                    library = vim.api.nvim_get_runtime_file("", true),
+                    checkThirdParty = false,
+                },
+                telemetry = { enable = false },
+            },
+        },
+    },
+
+
     ltex = {
         autostart = false,
         ltex = {
