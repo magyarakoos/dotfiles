b75e17f53c83ed133a6e662e66def077d511f736
magyarakoos <magyar.kende.22f@gmail.com>
2025-06-14 16:57:00 +0200

    sync: 2025-06-14 16:57:00

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -1,3 +1,21 @@
+local lspconfig = require('lspconfig')
+
+if not lspconfig.emmylua_ls then
+    lspconfig.emmylua_ls = {
+        default_config = {
+            cmd = { "false" },
+            filetypes = {},
+            root_dir = function() return nil end,
+            settings = {},
+        }
+    }
+end
+
+lspconfig.emmylua_ls.setup = function() end
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 require("lazy-bootstrap")
 
