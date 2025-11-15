18115345f86ddb9c46e25d6fc3bc8b93bc089ea7
magyarakoos <magyar.kende.22f@gmail.com>
2025-11-15 12:09:02 +0100

    sync: 2025-11-15 12:09:02

--- neovim/nvim/lua/lazy-plugins.lua
+++ neovim/nvim/lua/lazy-plugins.lua
@@ -97,5 +97,25 @@ require('lazy').setup({
     {
         "andweeb/presence.nvim",
     },
+    {
+        "kdheepak/lazygit.nvim",
+        lazy = true,
+        cmd = {
+            "LazyGit",
+            "LazyGitConfig",
+            "LazyGitCurrentFile",
+            "LazyGitFilter",
+            "LazyGitFilterCurrentFile",
+        },
+        -- optional for floating window border decoration
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        -- setting the keybinding for LazyGit with 'keys' is recommended in
+        -- order to load the plugin when the command is run for the first time
+        keys = {
+            { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+        }
+    },
     { import = 'plugins' },
 }, { change_detection = { notify = false }, defaults = { lazy = true } })
